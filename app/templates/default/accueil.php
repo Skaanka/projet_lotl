c0e2e905c92a7156ed6608f3326d38c9887ea635
AudDsgd <a.desgardin@free.fr>
2016-01-19 15:12:19 +0100

    comitage du home + acceuil modif

--- app/templates/default/accueil.php
+++ app/templates/default/accueil.php
@@ -2,12 +2,89 @@
 
 <?php $this->start('main_content') ?>
 <div id="homeContent" class="row col-md-12 no-padding">
-    <div class="row text-center">
-        <h1>Bonjour, <?php echo htmlspecialchars($value["prenom"]) ?> </h1>
-    </div>
-    <div>
-        <h1 class="text-center"><b>"– Vous êtes en retard !<br/><br/>– Un magicien n’est jamais en retard, ni en avance d’ailleurs. Il arrive précisément à l’heure prévue."</b></h1>
-        <h3 class="col-md-offset-8"><i>- Frodon Sacquet, Gandalf le Gris.</i></h3>
-    </div>
+    <section class="portfolio">
+      <div class="container">
+        <div class="row">
+
+          <ul class="portfolio-items list-unstyled" id="grid">
+
+            <li class="col-md-4 col-sm-4 col-xs-6" data-groups='["psn"]'>
+              <figure class="portfolio-item">
+                <a href="#">
+                  <img src="http://lorempixel.com/700/400/people/1" alt="Item 1" class="img-responsive">
+                </a>
+              </figure>
+            </li>
+
+            <li class="col-md-4 col-sm-4 col-xs-6" data-groups='["steam"]'>
+                <figure class="portfolio-item">
+                    <a href="#">
+                        <img src="http://lorempixel.com/700/400/people/7" alt="" class="img-responsive">
+                    </a>
+                </figure>
+            </li>
+
+           <li class="col-md-4 col-sm-4 col-xs-6" data-groups='["youtube"]'>
+              <figure class="portfolio-item">
+                  <a href="#">
+                      <img src="http://lorempicsum.com/futurama/700/400/1" alt="" class="img-responsive">
+                  </a>
+              </figure>
+            </li>
+
+            <li class="col-md-4 col-sm-4 col-xs-6" data-groups='["pinterest"]'>
+              <figure class="portfolio-item">
+                  <a href="#">
+                      <img src="http://lorempicsum.com/futurama/700/400/2" alt="" class="img-responsive">
+                  </a>
+              </figure>
+            </li>
+
+            <li class="col-md-4 col-sm-4 col-xs-6" data-groups='["simpsons", "people"]'>
+              <figure class="portfolio-item">
+                  <a href="#">
+                      <img src="http://lorempicsum.com/simpsons/700/400/1" alt="" class="img-responsive">
+                  </a>
+              </figure>
+            </li>
+
+            <li class="col-md-4 col-sm-4 col-xs-6" data-groups='["simpsons"]'>
+              <figure class="portfolio-item">
+                  <a href="#">
+                      <img src="http://lorempicsum.com/simpsons/700/400/3" alt="" class="img-responsive">
+                  </a>
+              </figure>
+            </li>
+
+            <li class="col-md-4 col-sm-4 col-xs-6" data-groups='["people"]'>
+                <figure class="portfolio-item">
+                    <a href="#">
+                        <img src="http://lorempixel.com/700/400/people/9" alt="" class="img-responsive">
+                    </a>
+                </figure>
+            </li>
+
+             <li class="col-md-4 col-sm-4 col-xs-6" data-groups='["simpsons"]'>
+              <figure class="portfolio-item">
+                  <a href="#">
+                      <img src="http://lorempicsum.com/simpsons/700/400/4" alt="" class="img-responsive">
+                  </a>
+              </figure>
+            </li>
+
+            <li class="col-md-4 col-sm-4 col-xs-6" data-groups='["futurama"]'>
+              <figure class="portfolio-item">
+                  <a href="#">
+                      <img src="http://lorempicsum.com/futurama/700/400/5" alt="" class="img-responsive">
+                  </a>
+              </figure>
+            </li>
+            <!-- sizer -->
+            <li class="col-md-4 col-sm-4 col-xs-6 shuffle_sizer"></li>
+            
+          </ul> <!--end portfolio grid -->
+          </div> <!--end row -->
+        </div> <!-- end container-->
+    </section>
 </div>	
 <?php $this->stop('main_content') ?>
