d7adc416a0e129b91b630786a8f6ce31a1a63692
Skaanka <gripon.gregoire@sfr.fr>
2016-01-21 18:07:31 +0100

    maj erreur ]

--- app/templates/inscription/inscription3.php
+++ app/templates/inscription/inscription3.php
@@ -20,42 +20,42 @@
                             <img class="icons col-sm-2" src="../../img/icons/PSN_logo.png">
                             <label for="psn" class="col-sm-2 control-label celSoc">PSN</label>
                             <div class="col-sm-8">
-                                <input type="text" class="form-control celSoc" id="psn" placeholder="Identifiant Playstation Network" name="reseaux_divertissement[psn">
+                                <input type="text" class="form-control celSoc" id="psn" placeholder="Identifiant Playstation Network" name="reseaux_divertissement[psn]">
                             </div>
                         </div>
                         <div class="row form-group">
                             <img class="icons col-sm-2" src="../../img/icons/Xbox_logo.png">
                             <label for="xbox" class="col-sm-2 control-label celSoc">XboxLive</label>
                             <div class="col-sm-8">
-                                <input type="text" class="form-control celSoc" id="xbox" placeholder="Identifiant XboxLive" name="reseaux_divertissement[xboxlive">
+                                <input type="text" class="form-control celSoc" id="xbox" placeholder="Identifiant XboxLive" name="reseaux_divertissement[xboxlive]">
                             </div>
                         </div>
                         <div class="row form-group">
                             <img class="icons col-sm-2" src="../../img/icons/Steam_logo.png">
                             <label for="steam" class="col-sm-2 control-label celSoc">Steam</label>
                             <div class="col-sm-8">
-                                <input type="text" class="form-control celSoc" id="steam" placeholder="Identifiant Steam" name="reseaux_divertissement[steam">
+                                <input type="text" class="form-control celSoc" id="steam" placeholder="Identifiant Steam" name="reseaux_divertissement[steam]">
                             </div>
                         </div>
                         <div class="row form-group">
                             <img class="icons col-sm-2" src="../../img/icons/battlenet_logo.png">
                             <label for="battlenet" class="col-sm-2 control-label celSoc">Battle.net</label>
                             <div class="col-sm-8">
-                                <input type="text" class="form-control celSoc" id="battlenet" placeholder="Identifiant Battle.net" name="reseaux_divertissement[battlenet">
+                                <input type="text" class="form-control celSoc" id="battlenet" placeholder="Identifiant Battle.net" name="reseaux_divertissement[battlenet]">
                             </div>
                         </div>
                         <div class="row form-group">
                             <img class="icons col-sm-2" src="../../img/icons/nintendo_logo.png">
                             <label for="nintendo" class="col-sm-2 control-label celSoc">Nintendo</label>
                             <div class="col-sm-8">
-                                <input type="text" class="form-control celSoc" id="nintendo" placeholder="Identifiant Nintendo" name="reseaux_divertissement[nintendo">
+                                <input type="text" class="form-control celSoc" id="nintendo" placeholder="Identifiant Nintendo" name="reseaux_divertissement[nintendo]">
                             </div>
                         </div>
                         <div class="row form-group">
                             <img class="icons col-sm-2" src="../../img/icons/origin1_logo.png">
                             <label for="origin" class="col-sm-2 control-label celSoc">Origin</label>
                             <div class="col-sm-8">
-                                <input type="text" class="form-control celSoc" id="origin" placeholder="Identifiant Origin" name="reseaux_divertissement[origin">
+                                <input type="text" class="form-control celSoc" id="origin" placeholder="Identifiant Origin" name="reseaux_divertissement[origin]">
                             </div>
                         </div>
                     </div>
@@ -68,28 +68,28 @@
                             <img class="icons col-sm-2" src="../../img/icons/linkedin_logo.png">
                             <label for="linkedin" class="col-sm-2 control-label celSoc">LinkedIn</label>
                             <div class="col-sm-8">
-                                <input type="text" class="form-control celSoc" id="linkedin" placeholder="LikedIn" name="reseaux_pro[linkedin">
+                                <input type="text" class="form-control celSoc" id="linkedin" placeholder="LikedIn" name="reseaux_pro[linkedin]">
                             </div>
                         </div>
                         <div class="row form-group">
                             <img class="icons col-sm-2" src="../../img/icons/viadeo_logo.png">
                             <label for="viadeo" class="col-sm-2 control-label celSoc">Viadeo</label>
                             <div class="col-sm-8">
-                                <input type="text" class="form-control celSoc" id="viadeo" placeholder="Viadeo" name="reseaux_pro[viadeo">
+                                <input type="text" class="form-control celSoc" id="viadeo" placeholder="Viadeo" name="reseaux_pro[viadeo]">
                             </div>
                         </div>
                         <div class="row form-group">
                             <img class="icons col-sm-2" src="../../img/icons/xing_logo.png">
                             <label for="xing" class="col-sm-2 control-label celSoc">Xing</label>
                             <div class="col-sm-8">
-                                <input type="text" class="form-control celSoc" id="xing" placeholder="Xing" name="reseaux_pro[xing">
+                                <input type="text" class="form-control celSoc" id="xing" placeholder="Xing" name="reseaux_pro[xing]">
                             </div>
                         </div>
                         <div class="row form-group">
                             <img class="icons col-sm-2" src="../../img/icons/muxi_logo.jpg">
                             <label for="muxy" class="col-sm-2 control-label celSoc">Muxy</label>
                             <div class="col-sm-8">
-                                <input type="text" class="form-control celSoc" id="muxy" placeholder="Muxy" name="reseaux_pro[muxy">
+                                <input type="text" class="form-control celSoc" id="muxy" placeholder="Muxy" name="reseaux_pro[muxy]">
                             </div>
                         </div>
                     </div>
@@ -102,70 +102,70 @@
                             <img class="icons col-sm-2" src="../../img/icons/FB_logo.png">
                             <label for="facebook" class="col-sm-2 control-label celSoc">Facebook</label>
                             <div class="col-sm-8">
-                                <input type="text" class="form-control celSoc" id="facebook" placeholder="Facebook" name="reseaux_social[facebook">
+                                <input type="text" class="form-control celSoc" id="facebook" placeholder="Facebook" name="reseaux_social[facebook]">
                             </div>
                         </div>
                         <div class="row form-group">
                             <img class="icons col-sm-2" src="../../img/icons/Twitter-logo.png">
                             <label for="Twitter" class="col-sm-2 control-label celSoc">Twitter</label>
                             <div class="col-sm-8">
-                                <input type="text" class="form-control celSoc" id="twitter" placeholder="Twitter" name="reseaux_social[twitter">
+                                <input type="text" class="form-control celSoc" id="twitter" placeholder="Twitter" name="reseaux_social[twitter]">
                             </div>
                         </div>
                         <div class="row form-group">
                             <img class="icons col-sm-2" src="../../img/icons/YouTube-logo.png">
                             <label for="youtube" class="col-sm-2 control-label celSoc">YouTube</label>
                             <div class="col-sm-8">
-                                <input type="text" class="form-control celSoc" id="youtube" placeholder="YouTube" name="reseaux_social[youtube">
+                                <input type="text" class="form-control celSoc" id="youtube" placeholder="YouTube" name="reseaux_social[youtube]">
                             </div>
                         </div>
                         <div class="row form-group">
                             <img class="icons col-sm-2" src="../../img/icons/googleplus_logo.png">
                             <label for="google" class="col-sm-2 control-label celSoc">Google+</label>
                             <div class="col-sm-8">
-                                <input type="text" class="form-control celSoc" id="google" placeholder="Google+" name="reseaux_social[google">
+                                <input type="text" class="form-control celSoc" id="google" placeholder="Google+" name="reseaux_social[google]">
                             </div>
                         </div>
                         <div class="row form-group">
                             <img class="icons col-sm-2" src="../../img/icons/Skype_logo.png">
                             <label for="skype" class="col-sm-2 control-label celSoc">Skype</label>
                             <div class="col-sm-8">
-                                <input type="text" class="form-control celSoc" id="skype" placeholder="Skype" name="reseaux_social[skype">
+                                <input type="text" class="form-control celSoc" id="skype" placeholder="Skype" name="reseaux_social[skype]">
                             </div>
                         </div>
                         <div class="row form-group">
                             <img class="icons col-sm-2" src="../../img/icons/instagram_logo.png">
                             <label for="instagram" class="col-sm-2 control-label celSoc">Instagram</label>
                             <div class="col-sm-8">
-                                <input type="text" class="form-control celSoc" id="instagram" placeholder="Instagram" name="reseaux_social[instagram">
+                                <input type="text" class="form-control celSoc" id="instagram" placeholder="Instagram" name="reseaux_social[instagram]">
                             </div>
                         </div>
                         <div class="row form-group">
                             <img class="icons col-sm-2" src="../../img/icons/pinterest-logo.png">
                             <label for="pinterest" class="col-sm-2 control-label celSoc">Pinterest</label>
                             <div class="col-sm-8">
-                                <input type="text" class="form-control celSoc" id="pinterest" placeholder="Pinterest" name="reseaux_social[pinterest">
+                                <input type="text" class="form-control celSoc" id="pinterest" placeholder="Pinterest" name="reseaux_social[pinterest]">
                             </div>
                         </div>
                         <div class="row form-group">
                             <img class="icons col-sm-2" src="../../img/icons/Deezer_logo.png">
                             <label for="deezer" class="col-sm-2 control-label celSoc">Deezer</label>
                             <div class="col-sm-8">
-                                <input type="text" class="form-control celSoc" id="deezer" placeholder="Deezer" name="reseaux_social[deezer">
+                                <input type="text" class="form-control celSoc" id="deezer" placeholder="Deezer" name="reseaux_social[deezer]">
                             </div>
                         </div>
                         <div class="row form-group">
                             <img class="icons col-sm-2" src="../../img/icons/Spotify_logo.png">
                             <label for="spotify" class="col-sm-2 control-label celSoc">Spotify</label>
                             <div class="col-sm-8">
-                                <input type="text" class="form-control celSoc" id="spotify" placeholder="Spotify" name="reseaux_social[spotify">
+                                <input type="text" class="form-control celSoc" id="spotify" placeholder="Spotify" name="reseaux_social[spotify]">
                             </div>
                         </div>
                         <div class="row form-group">
                             <img class="icons col-sm-2" src="../../img/icons/Viber-logo.png">
                             <label for="Viber" class="col-sm-2 control-label celSoc">Viber</label>
                             <div class="col-sm-8">
-                                <input type="text" class="form-control celSoc" id="viber" placeholder="Viber" name="reseaux_social[viber">
+                                <input type="text" class="form-control celSoc" id="viber" placeholder="Viber" name="reseaux_social[viber]">
                             </div>
                         </div>
 
