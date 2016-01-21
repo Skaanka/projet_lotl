3acbf9e356ccc3efe22ddce93303e17ade2b7d30
Dkangni <kangni.devis@yahoo.fr>
2016-01-21 11:59:21 +0100

    modification des 3 pages inscription, identique à la BDD

--- app/templates/default/inscription1.php
+++ app/templates/default/inscription1.php
@@ -28,15 +28,15 @@
                 <div class="row" id="espaceform">
                     <div class="form-group col-md-6">
                         <label for="name">Nom</label>
-                        <input type="text" class="form-control" id="name" placeholder="nom" name="inscription_1[nom]" value="<?php  if ( !empty($_SESSION['inscription_1']['nom'])) {echo $_SESSION['inscription_1']['nom'];}  ?>">
+                        <input type="text" class="form-control" id="name" placeholder="nom" name="membre[nom]">
                     </div>
                     <div class="form-group col-md-6">
                         <label for="firstname">Prenom</label>
-                        <input type="text" class="form-control" id="firstname" placeholder="prenom" name="inscription_1[prenom]" value="<?php  if ( !empty($_SESSION['inscription_1']['prenom'])) {echo $_SESSION['inscription_1']['prenom'];}  ?>">
+                        <input type="text" class="form-control" id="firstname" placeholder="prenom" name="membre[prenom]">
                     </div>
                     <div class="form-group col-md-6 col-md-offset-3">
                         <label for="password">Password</label>     <!-- Ajout d'un  champs confirmation mot de passe ? -->
-                        <input type="text" class="form-control" id="password" placeholder="Password" name="inscription_1[mot_de_passe]" value="">
+                        <input type="text" class="form-control" id="password" placeholder="Password" name="membre[mot_de_passe]" value="">
                     </div>
                 </div>
                 
@@ -44,44 +44,44 @@
                 <div class="row">
                     <div class="form-group col-md-12">
                         <label for="birthD">date de naissance</label>
-                        <input type="date" class="form-control" id="birthD" placeholder="date de naissance" name="inscription_1[ddn]" value="<?php  if ( !empty($_SESSION['inscription_1']['ddn'])) { echo $_SESSION['inscription_1']['ddn'];}  ?>">
+                        <input type="date" class="form-control" id="birthD" placeholder="date de naissance" name="membre[ddn]">
                     </div>
                     <div class="form-group col-md-12">
                         <label for="adresse">Adresse</label>
-                        <input type="text" class="form-control" id="adresse" placeholder="adresse" name="inscription_1[adresse]" value="<?php  if ( !empty($_SESSION['inscription_1']['adresse'])) { echo $_SESSION['inscription_1']['adresse'];}  ?>">
+                        <input type="text" class="form-control" id="adresse" placeholder="adresse" name="membre[adresse]">
                     </div>
                 </div>
                 
                 <div class="row">
                     <div class="form-group col-md-6">
                         <label for="cp">Code Postal</label>
-                        <input type="text" class="form-control" id="cp" placeholder="code postal" name="inscription_1['cp']" value="<?php  if ( !empty($_SESSION['inscription_1']['cp'])) { echo $_SESSION['inscription_1']['cp'];}  ?>">
+                        <input type="text" class="form-control" id="cp" placeholder="code postal" name="membre['cp']">
                     </div>
                     <div class="form-group col-md-6">
                         <label for="city">Ville</label>
-                        <input type="text" class="form-control" id="city" placeholder="Ville" name="inscription_1['ville']" value="<?php  if ( !empty($_SESSION['inscription_1']['ville'])) { echo $_SESSION['inscription_1']['ville'];}  ?>">
+                        <input type="text" class="form-control" id="city" placeholder="Ville" name="membre['ville']">
                     </div>
                 </div>
                     
                 <div class="row">
                     <div class="form-group col-md-6">
                         <label for="email">E-mail</label>
-                        <input type="text" class="form-control" id="email" placeholder="E-mail" name="inscription_1['mail']" value="">
+                        <input type="text" class="form-control" id="email" placeholder="E-mail" name="membre['mail']" value="">
                     </div>
                     <div class="form-group col-md-6">
                         <label for="phone">Téléphone mobile</label>
-                        <input type="text" class="form-control" id="phone" placeholder="Téléphone mobile" name="inscription_1['phone']" value="<?php  if ( !empty($_SESSION['inscription_1']['phone'])) { echo $_SESSION['inscription_1']['phone'];}  ?>">
+                        <input type="text" class="form-control" id="phone" placeholder="Téléphone mobile" name="membre['phone']">
                     </div>
                 </div>
                 
                 <div class="row">
                     <div class="form-group col-md-6">
                         <label for="webSite">Site web-perso</label>
-                        <input type="text" class="form-control" id="webSite" placeholder="Site web-perso" name="inscription_1['siteWeb']" value="<?php  if ( !empty($_SESSION['inscription_1']['siteWeb'])) { echo $_SESSION['inscription_1']['siteWeb'];}  ?>">
+                        <input type="text" class="form-control" id="webSite" placeholder="Site web-perso" name="membre['siteWeb']">
                     </div>
                     <label id="spanAvatar"for="pics" class=" col-md-3">Avatar</label>
                     <div class="col-md-6">
-                        <input type="file" class="" id="pics" name="inscription_1['avatar']" value="<?php  if ( !empty($_SESSION['inscription_1']['avatar'])) { echo $_SESSION['inscription_1']['avatar'];}  ?>">
+                        <input type="file" class="" id="pics" name="membre['avatar']">
                     </div>
                 </div>
                 
