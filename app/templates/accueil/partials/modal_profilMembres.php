c2f09a50c41bead7dc10f142e5fda5a3d38296bb
Skaanka <gripon.gregoire@sfr.fr>
2016-01-31 19:38:23 +0100

    correction duplication d'id

--- app/templates/accueil/partials/modal_profilMembres.php
+++ app/templates/accueil/partials/modal_profilMembres.php
@@ -3,7 +3,7 @@
 		<div class="modal-content">
 			<div class="modal-header">
 				<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-				<h4 class="modal-title" id="myModalLabel">Le profil de <?php echo $membre['prenom']?> <?php echo $membre['nom']?>.</h4>
+				<h4 class="modal-title" id="myModalLabel<?php echo $membre['id']?>">Le profil de <?php echo $membre['prenom']?> <?php echo $membre['nom']?>.</h4>
 			</div>
 			<div class="modal-body">
 
