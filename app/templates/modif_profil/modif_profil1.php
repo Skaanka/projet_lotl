b8c3aef4485a42f04ee05bef7ad4876321a770a2
Skaanka <gripon.gregoire@sfr.fr>
2016-01-31 15:16:13 +0100

    ajout redirection accueil

--- app/templates/modif_profil/modif_profil1.php
+++ app/templates/modif_profil/modif_profil1.php
@@ -11,7 +11,7 @@
 				<span class="icon-bar"></span>
 				<span class="icon-bar"></span>
 			</button>
-			<a class="navbar-brand" href="#">Lord of the links</a>
+			<a class="navbar-brand" href="<?php echo $this->url('accueil') ?>">Lord of the links</a>
             
             <!--    debut "oeil de sauron" -->
             
