7a23fc0f3b316d1aed5290124f34c18f28b71b25
Skaanka <gripon.gregoire@sfr.fr>
2016-02-01 18:47:48 +0100

    suppression API KEY SendGrid

--- app/Helper/Helper.php
+++ app/Helper/Helper.php
@@ -9,7 +9,7 @@ class Helper
 
 	public static function mail($destinataire, $objet, $message) 
 	{
-		$sendgrid = new SendGrid("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"); // API-KEY
+		$sendgrid = new SendGrid("MON API KEY"); // API-KEY
 		$email = new SendGrid\Email();
 
 		$email->addTo($destinataire)
